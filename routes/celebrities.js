ed7d22c32d8fd19da86f519401aa7d7ca800dee7
J Nicholson <jenicholson.nc@gmail.com>
2018-08-27 09:39:17 +0200

    iteration 4

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -10,6 +10,23 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/new', (req, res, next) => {
+  res.render('new');
+});
+
+router.post('/new', (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  const newCeleb = new Celebrity({ name, occupation, catchPhrase });
+  newCeleb
+    .save()
+    .then(celebrity => {
+      res.redirect('/celebrities');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
 router.get('/:id', (req, res, next) => {
   Celebrity.findById(req.params.id).then(celebrityFromDb => {
     res.render('show', celebrityFromDb);
